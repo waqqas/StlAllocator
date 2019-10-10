135d4c1b9574f46fb3e6cc89d31cde79f608c858
Waqqas Jabbar <waqqas.jabbar@emumba.com>
2019-10-10 15:38:27 +0500

    Added benchmark for std::unordered_map

--- benchmarks/benchmark.cpp
+++ benchmarks/benchmark.cpp
@@ -14,6 +14,19 @@ static void BM_security_map(benchmark::State &state)
   }
 }
 
+static void BM_unordered_map(benchmark::State &state)
+{
+  std::unordered_map<int32_t, uint64_t> map;
+  int32_t key = 0;
+  int64_t value = 0;
+  for (auto _ : state)
+  {
+    map.insert({key, value});
+    ++key;
+  }
+}
+
 BENCHMARK(BM_security_map);
+BENCHMARK(BM_unordered_map);
 
 BENCHMARK_MAIN();
